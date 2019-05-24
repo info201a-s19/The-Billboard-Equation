ccf8526dda8e036b86bdfc5d9a665ccd84a18057
Monica <monimonmon@D-10-18-162-0.dhcp4.washington.edu>
2019-05-23 23:59:29 -0700

    key and mode

--- summaryInformation.R
+++ summaryInformation.R
@@ -1,14 +1,28 @@
 library("dplyr")
 
+# Load csv file
 information <- read.csv("csvFiles/SpotifyAudioFeaturesApril2019.csv")
 
-
-summary_info <- information %>% 
+# Filter undesired information such as podcasts and create new variable -- Key and Mode
+filtered_info <- information %>% 
   select(artist_name, track_name, energy, tempo, valence, popularity, speechiness, key, mode) %>% 
   filter(popularity != 0, speechiness < 0.66, key != -1) %>% 
-  mutate(paste0(key, " ", mode))
+  mutate(paste0(key, mode))
 
-  
+# Group the data by Key and Mode and arranging the data by frequency
+data <- summary_info %>%
+  group_by(paste0(key, mode)) %>%
+  summarize(freq = n()) %>%
+  arrange(desc(freq))
+data$`paste0(key, mode)` <- c("C Major", "D Major", "C1 Major", "E Major", "E1 Major", "D1 Major",
+                              "G1 Minor", "A Major", "E1 Minor", "A Minor", "G Minor", "F1 Minor",
+                              "B Minor", "D Minor", "B Major", "F1 Major", "G1 Major", "G Major",
+                              "C1 Minor", "C Minor", "E Minor", "D1 Minor", "F Major", "F Minor")
+
+## Final Summary Table for Key and Mode vs Frequency
+Key.Mode.Freq <- data.frame("Key and Mode" = data$`paste0(key, mode)`,
+                            "Number of Songs" = data$freq)
+
 get_summary_info <- function(dataset) {
     ret <- list()
     ret$size <- nrow(dataset)
@@ -28,11 +42,6 @@ get_summary_info <- function(dataset) {
 
 get_summary_info(summary_info)
 
+doubles_filtered <- summary_info %>%
+  distinct(artist_name, track_name, .keep_all = TRUE)
 
-##This isnt right i dont think
- filtering_doubles <- function(dataset) {
-   f <- filter(dataset, duplicated(dataset$artist_name), duplicated(dataset$artist_name))
-   return(f)
- }
-
-filtering_doubles(summary_info)
